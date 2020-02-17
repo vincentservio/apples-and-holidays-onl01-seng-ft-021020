691d51cf688766d4531ed9e1651a76ddec548d83
Vincent Servio <vincentservio@gmail.com>
2020-02-17 07:00:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
  holiday_hash.each do |season, holiday|
    puts "#{season.to_s.capitalize}:"
    holiday.each do |e, s|
-     puts " #{e.to_s.split("_").map { |c| c.capitalize}.join("") }: #{s.join(",")}"
+     puts " #{e.to_s.split("_").map { |c| c.capitalize}.join(" ") }: #{s.join(",")}"
    end 
 end
 end
