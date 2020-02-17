b6b5b46628f51d54a34ab72eee14825259983c1a
Vincent Servio <vincentservio@gmail.com>
2020-02-17 06:41:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
      {|c| c.capitalize}.join(" ") }: #{s.join(",")}"
    end 
 end
-
+end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
